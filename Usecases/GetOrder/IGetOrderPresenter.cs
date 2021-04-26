d030fd6e6f150def1ca253d8b466151b55539700
christianbergau <cbergau86@gmail.com>
2021-04-26 20:49:07 +0200

    Clean Arc

---
+++ Usecases/GetOrder/IGetOrderPresenter.cs
@@ -0,0 +1,7 @@
+namespace OrderManagement.Usecases.GetOrder
+{
+    public interface IGetOrderPresenter
+    {
+        public void Present(OrderResponseModel order);
+    }
+}
---
+++ Usecases/GetOrder/OrderResponseModel.cs
@@ -0,0 +1,8 @@
+namespace OrderManagement.Usecases.GetOrder
+{
+    public class OrderResponseModel
+    {
+        public int state;
+        public string id;
+    }
+}
